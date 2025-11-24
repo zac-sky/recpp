7f840f92d3ea660ab6c74fe8d6e590fe84b0e6d8
zac-sky <2027226127@qq.com>
2025-11-24 16:25:27 +0800

    feat: Add test cases for turn right command

--- Experiment_Student/tests/ExecutorTest.cpp
+++ Experiment_Student/tests/ExecutorTest.cpp
@@ -159,4 +159,60 @@ TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
     ASSERT_EQ(executor->Query(), target);
 }
 
+// 测试用例11, 测试Execute方法,朝向E,起点(0,0),执行R命令,期望结果为(0,0,S)
+TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("R");
+
+    // then
+    const Pose target = {0, 0, 'S'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+// 测试用例12, 测试Execute方法,朝向W,起点(0,0),执行R命令,期望结果为(0,0,N)
+TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+    // when
+    executor->Execute("R");
+
+    // then
+    const Pose target = {0, 0, 'N'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+// 测试用例13, 测试Execute方法,朝向N,起点(0,0),执行R命令,期望结果为(0,0,E)
+TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+    // when
+    executor->Execute("R");
+
+    // then
+    const Pose target = {0, 0, 'E'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+// 测试用例14, 测试Execute方法,朝向S,起点(0,0),执行R命令,期望结果为(0,0,W)
+TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+    // when
+    executor->Execute("R");
+
+    // then
+    const Pose target = {0, 0, 'W'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
 } // namespace adas
